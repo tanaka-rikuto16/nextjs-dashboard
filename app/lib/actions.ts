c968ec6479714a69fe46d3a90bf72f6c63461ae0
Tanaka Rikuto <r-tanaka@c-rcs.jp>
2026-04-28 17:39:38 +0900

    Add authentication with NextAuth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
@@ -23,6 +25,25 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true});
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+){
+    try {
+        await signIn('credentials', formData);
+    }catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                    default:
+                        return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
 
 export type State = {
   errors?: {
